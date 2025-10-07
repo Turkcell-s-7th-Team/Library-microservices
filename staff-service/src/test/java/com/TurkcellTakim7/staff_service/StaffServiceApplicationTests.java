664de6f62f0d6c0bf99ef9054e0d795ee14c5b1e
Eren Sarialp <erensrlp@gmail.com>
2025-10-07 14:24:41 +0300

    first commit

---
+++ staff-service/src/test/java/com/TurkcellTakim7/staff_service/StaffServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TurkcellTakim7.staff_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
