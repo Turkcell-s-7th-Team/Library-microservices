4ae0573b7ea370ea1363565ad5adda6e2eebef21
Doğaç Ulğaç <ulgac1907@gmail.com>
2025-11-20 16:24:43 +0300

    Configure Kafka bindings for loan creation events in application.yml, setting up the default binder and specifying the loanCreated input channel.

---
+++ book-service/src/main/java/com/TurkcellTakim7/book_service/infrastructure/messaging/listener/LoanEventListener.java
@@ -0,0 +1,29 @@
+package com.TurkcellTakim7.book_service.infrastructure.messaging.listener;
+
+import java.util.function.Consumer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import com.TurkcellTakim7.book_service.domain.services.BookDomainService;
+import com.TurkcellTakim7.book_service.domain.valueobjects.BookId;
+import com.TurkcellTakim7.book_service.infrastructure.messaging.event.LoanCreatedEvent;
+
+@Component
+public class LoanEventListener {
+
+  private final BookDomainService bookDomainService;
+
+  public LoanEventListener(BookDomainService bookDomainService) {
+    this.bookDomainService = bookDomainService;
+  }
+
+  @Bean
+  public Consumer<LoanCreatedEvent> loanCreated() {
+    return event -> {
+      System.out.println("Received LoanCreatedEvent for bookId: " + event.bookId());
+      bookDomainService.borrowBook(new BookId(event.bookId()));
+    };
+  }
+
+}
